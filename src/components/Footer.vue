8ad71c4b5f4d09c35732784abb6d61e8a0ce47b9
Santi Jimenez <santiagojimenezisc@gmail.com>
2021-08-28 18:59:27 -0500

    Se agrega columnas de bt sin eje x

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,16 @@
 <template>
     <div id="footer">
-
+        <div class="row">
+            <div class="col-4">
+                Santi
+            </div>
+            <div class="col-4">
+                Navegación
+            </div>
+            <div class="col-4">
+                Redes sociales
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -10,6 +20,7 @@ export default {
 </script>
 <style scoped>
 #footer{
+    overflow-x:hidden;
     background-color:#0085FF;
     max-height: 200px;
     max-width: 100vw;
