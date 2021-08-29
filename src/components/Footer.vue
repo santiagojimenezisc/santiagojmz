525c9123ba056be894af8ed79f4f10c623c36239
Santi Jimenez <santiagojimenezisc@gmail.com>
2021-08-29 02:46:02 -0500

    Agrego footer beta

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,28 @@
 <template>
-  <div id="footer" class="">
+  <div id="footer">
     <footer class="container">
       <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="col-lg-4 col-md-4 col-sm-6">
           <div class="logo-footer">
             <h2>Santi</h2>
             <h4>Desarrollador Frontend</h4>
           </div>
         </div>
-        <div class="col-lg-4 col-md-4 col-sm-12 text-center">Navegación</div>
-        <div class="col-lg-4 col-md-4 col-sm-12 text-center">Redes sociales</div>
+        <div class="col-lg-4 col-md-4 col-sm-6 text-center footer-nav">
+          <a href="">Sobre mí</a>
+          <a href="">Portafolio</a>
+          <a href="">Contacto</a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-12 text-center footer-redes">
+          <a href=""><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+          <a href=""><font-awesome-icon :icon="['fab', 'linkedin']" /></a>
+          <a href=""><font-awesome-icon :icon="['fab', 'dribbble']" /></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="made-by">
+          <h6>Hecha con pasión by Santi.</h6>
+        </div>
       </div>
     </footer>
   </div>
@@ -20,6 +33,29 @@ export default {
 };
 </script>
 <style scoped>
+.made-by{
+    display: flex;
+    justify-content: center;
+    margin: 75px 0px 0px 0px;
+}
+.col-sm-12 {
+  margin: 5px 0px;
+}
+.footer-redes {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 25px;
+}
+.footer-redes a {
+  font-size: 24px !important;
+}
+.footer-nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
 #footer {
   display: flex;
   align-items: center;
@@ -28,25 +64,50 @@ export default {
   width: 100%;
   overflow-y: hidden;
   background-color: #0085ff;
-  max-height: 200px;
   color: #fff;
   position: absolute;
   bottom: 0;
   left: 0;
-  padding: 50px 0;
+  padding: 75px 0px 25px 0px;
 }
 .logo-footer {
   text-align: left;
 }
+a {
+  color: #fff;
+  text-decoration: none;
+  transition: 0.4s;
+}
+a:hover {
+  color: #00386b;
+  transition: 0.4s;
+}
 @media only screen and (max-width: 768px) {
-  .logo-footer {
-  text-align: center;
+
+  .footer-nav {
+    flex-direction: column;
+    align-items: flex-end;
+  }
+
+.footer-redes {
+  justify-content: start;
+
 }
 }
 
+h2 {
+  font-weight: 700;
+  font-size: 36px;
+}
 h4 {
   font-size: 16px;
-  font-weight: 500;
-  color:#C3E2FF;
+  font-weight: 300;
+  color: #c3e2ff;
+}
+#footer h6 {
+  font-size: 14px;
+  color: #87c5ff;
+  text-align: center;
+  font-weight: 400;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-createApp(App).use(router).mount("#app");
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faDribbble} from '@fortawesome/free-brands-svg-icons'
+
+
+
+library.add(faTwitter,faDribbble,faLinkedin)
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(router).mount("#app");
